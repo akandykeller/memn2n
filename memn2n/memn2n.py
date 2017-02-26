4106c8d76db071eb183883eaa4e96a5f65477956
Andy Keller <akandykeller@gmail.com>
2017-02-26 00:43:32 -0800

    Add FFN sim function and RELU memory update

--- memn2n/memn2n.py
+++ memn2n/memn2n.py
@@ -53,12 +53,17 @@ def add_gradient_noise(t, stddev=1e-3, name=None):
         gn = tf.random_normal(tf.shape(t), stddev=stddev)
         return tf.add(t, gn, name=name)
 
+
+def fprop(X, w_1, w_2, b_1, b_2):
+    h = tf.nn.tanh(tf.matmul(X, w_1) + b_1)  
+    yhat = tf.matmul(h, w_2) + b_2
+    return yhat
+
 class MemN2N(object):
     """End-To-End Memory Network."""
     def __init__(self, batch_size, vocab_size, sentence_size, memory_size, embedding_size,
         hops=3,
         max_grad_norm=40.0,
-        nonlin=None,
         initializer=tf.random_normal_initializer(stddev=0.1),
         encoding=position_encoding,
         session=tf.Session(),
@@ -105,19 +110,18 @@ class MemN2N(object):
         self._embedding_size = embedding_size
         self._hops = hops
         self._max_grad_norm = max_grad_norm
-        self._nonlin = nonlin
         self._init = initializer
         self._name = name
 
         self._build_inputs()
         self._build_vars()
 
-        self._opt = tf.train.GradientDescentOptimizer(learning_rate=self._lr)
+        self._opt = tf.train.AdamOptimizer(learning_rate=0.001) # tf.train.GradientDescentOptimizer(learning_rate=self._lr)
 
         self._encoding = tf.constant(encoding(self._sentence_size, self._embedding_size), name="encoding")
 
         # cross entropy
-        logits, self.q_emb, self.u_0 = self._inference(self._stories, self._queries) # (batch_size, vocab_size)
+        logits = self._inference(self._stories, self._queries) # (batch_size, vocab_size)
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, tf.cast(self._answers, tf.float32), name="cross_entropy")
         cross_entropy_sum = tf.reduce_sum(cross_entropy, name="cross_entropy_sum")
 
@@ -165,16 +169,24 @@ class MemN2N(object):
             A = tf.concat(0, [ nil_word_slot, self._init([self._vocab_size-1, self._embedding_size]) ])
             C = tf.concat(0, [ nil_word_slot, self._init([self._vocab_size-1, self._embedding_size]) ])
 
+            self.W_1 = tf.Variable(self._init([7 * self._embedding_size, 3 * self._embedding_size]), name="W_1")
+            self.W_2 = tf.Variable(self._init([3 * self._embedding_size, 1]), name="W_2")
+            self.b_1 = tf.Variable(self._init([1, 3 * self._embedding_size]), name="b_1")
+            self.b_2 = tf.Variable(self._init([1, 1]), name="b_2")
+
             self.A_1 = tf.Variable(A, name="A")
 
             self.C = []
+            self.H = []
+            self.H_b = []
 
             for hopn in range(self._hops):
                 with tf.variable_scope('hop_{}'.format(hopn)):
-                    self.C.append(tf.Variable(C, name="C"))
+                    if hopn < self._hops - 1:
+                        self.C.append(tf.Variable(C, name="C"))
 
-            # Dont use projection for layerwise weight sharing
-            # self.H = tf.Variable(self._init([self._embedding_size, self._embedding_size]), name="H")
+                    self.H.append(tf.Variable(self._init([self._embedding_size * 3, self._embedding_size]), name="H"))
+                    self.H_b.append(tf.Variable(self._init([1, self._embedding_size])))
 
             # Use final C as replacement for W
             # self.W = tf.Variable(self._init([self._embedding_size, self._vocab_size]), name="W")
@@ -185,8 +197,8 @@ class MemN2N(object):
         with tf.variable_scope(self._name):
             # Use A_1 for thee question embedding as per Adjacent Weight Sharing
             q_emb = tf.nn.embedding_lookup(self.A_1, queries)
-            u_0 = tf.reduce_sum(q_emb * self._encoding, 1)
-            u = [u_0]
+            q = tf.reduce_sum(q_emb * self._encoding, 1)
+            u = [q]
 
             for hopn in range(self._hops):
                 if hopn == 0:
@@ -199,30 +211,51 @@ class MemN2N(object):
                         m_A = tf.reduce_sum(m_emb_A * self._encoding, 2)
 
                 # hack to get around no reduce_dot
-                u_temp = tf.transpose(tf.expand_dims(u[-1], -1), [0, 2, 1])
-                dotted = tf.reduce_sum(m_A * u_temp, 2)
+                # u_temp = tf.transpose(tf.expand_dims(u[-1], -1), [0, 2, 1])
+                # dotted = tf.reduce_sum(m_A * u_temp, 2)
+
+                m_A_sentences = tf.unpack(m_A, axis=1)
+                sims = []
+
+                for i, sentence in enumerate(m_A_sentences):
+                    reuse = None if (i == 0 and hopn == 0) else True                        
+
+                    with tf.variable_scope('ffn_sim', reuse=reuse):
+                        z = [u[-1], q, sentence, u[-1] * sentence, u[-1] * q, tf.abs(u[-1] - sentence), tf.abs(u[-1] - q)]
+
+                        um = tf.concat(1, z)
+
+                        um_sim = fprop(um, self.W_1, self.W_2, self.b_1, self.b_2)
+                        sims.append(um_sim)
+
+                sims_mat = tf.pack(sims)
 
                 # Calculate probabilities
-                probs = tf.nn.softmax(dotted)
+                probs = tf.nn.softmax(sims_mat)
 
-                probs_temp = tf.transpose(tf.expand_dims(probs, -1), [0, 2, 1])
+                probs_temp = tf.transpose(probs [1, 2, 0])
 
-                with tf.variable_scope('hop_{}'.format(hopn)):
-                    m_emb_C = tf.nn.embedding_lookup(self.C[hopn], stories)
-                m_C = tf.reduce_sum(m_emb_C * self._encoding, 2)
+                # with tf.variable_scope('hop_{}'.format(hopn)):
+                #     m_emb_C = tf.nn.embedding_lookup(self.C[hopn], stories)
+                # m_C = tf.reduce_sum(m_emb_C * self._encoding, 2)
 
+                # c_temp = tf.transpose(m_C, [0, 2, 1])
 
-                c_temp = tf.transpose(m_C, [0, 2, 1])
-                o_k = tf.reduce_sum(c_temp * probs_temp, 2)
+                with tf.variable_scope('hop_{}'.format(hopn - 1)):
+                    m_emb_A = tf.nn.embedding_lookup(self.C[hopn - 1], stories)
+                m_A = tf.reduce_sum(m_emb_A * self._encoding, 2)
+
+                a_temp = tf.transpose(m_A, [0, 2, 1])
+                o_k = tf.reduce_sum(a_temp * probs_temp , 2)
 
                 # Dont use projection layer for adj weight sharing
                 # u_k = tf.matmul(u[-1], self.H) + o_k
 
-                u_k = u[-1] + o_k
+                with tf.variable_scope('hop_{}'.format(self._hops)):
+                    u_k = tf.matmul(tf.concat(1, [u[-1], o_k, q]), self.H[hopn]) + self.H_b[hopn] 
 
                 # nonlinearity
-                if self._nonlin:
-                    u_k = nonlin(u_k)
+                u_k = tf.nn.relu(u_k)
 
                 u.append(u_k)
 
@@ -243,7 +276,6 @@ class MemN2N(object):
         """
         feed_dict = {self._stories: stories, self._queries: queries, self._answers: answers, self._lr: learning_rate}
         loss, _ = self._sess.run([self.loss_op, self.train_op], feed_dict=feed_dict)
-        q_emb, u_0 = self._sess.run([self.q_emb, self.u_0], feed_dict=feed_dict)
 
         # print "q_emb:  " 
         # print "***"*100
