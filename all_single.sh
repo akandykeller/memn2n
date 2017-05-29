914c61f983c82c01d382316df6f38c1908913d74
Andy Keller <akandykeller@gmail.com>
2017-05-29 13:32:35 -0700

    Add test script

---
+++ all_single.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+for iter in {1..10}
+do
+    for t in {1..20}
+    do
+        echo "Beginning iter $iter, task $t. training" 
+        python single.py --task_id $t --data_dir data/tasks_1-20_v1-2/en/ 
+    done 
+done
